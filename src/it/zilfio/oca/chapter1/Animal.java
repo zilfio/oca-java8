026d577a31845c99e4ea998f224b7d4fca583b48
zilfio <zilfio88@gmail.com>
2018-12-03 15:41:46 +0100

    Aggiunte classi

---
+++ src/it/zilfio/oca/chapter1/Animal.java
@@ -0,0 +1,14 @@
+package it.zilfio.oca.chapter1;
+
+public class Animal {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/it/zilfio/oca/chapter1/Zoo.java
@@ -0,0 +1,9 @@
+package it.zilfio.oca.chapter1;
+
+public class Zoo {
+
+    public static void main(String[] args) {
+        System.out.println("Zoo!");
+    }
+
+}
